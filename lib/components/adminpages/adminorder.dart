411d8f6c24265c6e8ff5a46384efbec4661c6959
Eishal Cyril <eishalcyril@cufa.ie>
2025-02-23 05:46:10 +0530

    admin-change

--- lib/components/adminpages/adminorder.dart
+++ lib/components/adminpages/adminorder.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:laundry_app/common/network/newapiservice.dart';
 import 'package:laundry_app/config.dart';
-import 'package:lottie/lottie.dart'; // Import your config file
+import 'package:lottie/lottie.dart';
 
 class AdminOrdersPage extends StatefulWidget {
   const AdminOrdersPage({super.key});
@@ -31,17 +32,17 @@ class _AdminOrdersPageState extends State<AdminOrdersPage> {
             color: Colors.white,
           )
         ],
-        backgroundColor: primaryColor, // Use primaryColor from config
+        backgroundColor: primaryColor,
       ),
       body: FutureBuilder(
         future: NewApiService().getAdminOrders(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-             return Center(
-                        child: Lottie.asset('assets/loading-washing.json',
-                            height: MediaQuery.of(context).size.height * .5,
-                            width: MediaQuery.of(context).size.width * .5),
-                      );
+            return Center(
+              child: Lottie.asset('assets/loading-washing.json',
+                  height: MediaQuery.of(context).size.height * .5,
+                  width: MediaQuery.of(context).size.width * .5),
+            );
           }
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
@@ -57,94 +58,105 @@ class _AdminOrdersPageState extends State<AdminOrdersPage> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                child: ListTile(
-                  title: Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: secondaryColor)),
-                    // margin: EdgeInsets.all(4),
-                    padding: EdgeInsets.all(6),
-
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Order',
-                          style: TextStyle(
-                            color: secondaryColor, // Use txtColor from config
-                            fontWeight: FontWeight.bold,
-                          ),
+                child: Stack(
+                  children: [
+                    ListTile(
+                      title: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: secondaryColor)),
+                        padding: EdgeInsets.all(6),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Order',
+                              style: TextStyle(
+                                color: secondaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '#${order['id'].toString().substring(0, 8)}',
+                              style: TextStyle(
+                                color: secondaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          '#${order['id'].toString().substring(0, 8)}',
-                          style: TextStyle(
-                            color: secondaryColor, // Use txtColor from config
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                      ),
+                      subtitle: Table(
+                        columnWidths: const {
+                          0: FixedColumnWidth(120),
+                          1: FixedColumnWidth(20),
+                          2: FlexColumnWidth(),
+                        },
+                        children: [
+                          _buildTableRow('Service', order['serviceName']),
+                          _buildTableRow(
+                              'Material Type', order['materialType']),
+                          _buildTableRow(
+                              'Quantity', order['quantity'].toString()),
+                          _buildTableRow('Additional Description',
+                              order['additionalDescription']),
+                          _buildTableRowwithColor(
+                              'Status',
+                              _getStatusText(order['status']),
+                              _getStatusColor(order['status'])),
+                          _buildTableRow(
+                              'Delivery',
+                              DateFormat('yyyy-MM-dd HH:mm').format(
+                                  DateTime.parse(order['expectedDeliveryDate'])
+                                      .toLocal())),
+                        ],
+                      ),
                     ),
-                  ),
-                  subtitle: Table(
-                    columnWidths: const {
-                      0: FixedColumnWidth(120), // Fixed width for labels
-                      1: FixedColumnWidth(20), // Fixed width for colons
-                      2: FlexColumnWidth(), // Flexible width for values
-                    },
-                    children: [
-                      _buildTableRow('Service', order['serviceName']),
-                      _buildTableRow('Material Type', order['materialType']),
-                      _buildTableRow('Quantity', order['quantity'].toString()),
-                      _buildTableRow('Additional Description',
-                          order['additionalDescription']),
-                      _buildTableRowwithColor(
-                          'Status',
-                          _getStatusText(order['status']),
-                          _getStatusColor(order['status'])),
-                      _buildTableRow('Delivery', order['expectedDeliveryDate']),
-                    ],
-                  ),
-                  trailing: PopupMenuButton<int>(
-                    color: primaryColor,
-                    elevation: 8,
-                    onSelected: (status) async {
-                      if (_isUpdating) return;
-                      _isUpdating = true; // Set the flag to true
+                    Positioned(
+                      right: 8,
+                      top: MediaQuery.of(context).size.height * .06,
+                      child: PopupMenuButton<int>(
+                        color: primaryColor,
+                        elevation: 8,
+                        onSelected: (status) async {
+                          if (_isUpdating) return;
+                          _isUpdating = true;
 
-                      await NewApiService()
-                          .updateOrderStatus(
-                        orderId: order['id'],
-                        status: status,
-                      )
-                          .then((_) {
-                        setState(() {});
-                      }).catchError((error) {
-                        // Handle error if needed
-                      }).whenComplete(() {
-                        _isUpdating =
-                            false; // Reset the flag after the operation
-                      });
-                    },
-                    itemBuilder: (context) => [
-                      PopupMenuItem(
-                          value: 1,
-                          child: Text(
-                            'Processing',
-                            style: TextStyle(color: txtColor),
-                          )),
-                      PopupMenuItem(
-                          value: 2,
-                          child: Text(
-                            'In Progress',
-                            style: TextStyle(color: txtColor),
-                          )),
-                      PopupMenuItem(
-                          value: 3,
-                          child: Text(
-                            'Completed',
-                            style: TextStyle(color: txtColor),
-                          )),
-                    ],
-                  ),
+                          await NewApiService()
+                              .updateOrderStatus(
+                                orderId: order['id'],
+                                status: status,
+                              )
+                              .then((_) {
+                                setState(() {});
+                              })
+                              .catchError((error) {})
+                              .whenComplete(() {
+                                _isUpdating = false;
+                              });
+                        },
+                        itemBuilder: (context) => [
+                          PopupMenuItem(
+                              value: 1,
+                              child: Text(
+                                'Processing',
+                                style: TextStyle(color: txtColor),
+                              )),
+                          PopupMenuItem(
+                              value: 2,
+                              child: Text(
+                                'In Progress',
+                                style: TextStyle(color: txtColor),
+                              )),
+                          PopupMenuItem(
+                              value: 3,
+                              child: Text(
+                                'Completed',
+                                style: TextStyle(color: txtColor),
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
@@ -158,17 +170,15 @@ class _AdminOrdersPageState extends State<AdminOrdersPage> {
     return TableRow(
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: Text(label, style: TextStyle(color: secondaryColor)),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
-          child: Text(':',
-              style:
-                  TextStyle(color: secondaryColor)), // Colon in its own column
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Text(':', style: TextStyle(color: secondaryColor)),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: Text(value, style: TextStyle(color: secondaryColor)),
         ),
       ],
@@ -179,23 +189,23 @@ class _AdminOrdersPageState extends State<AdminOrdersPage> {
     return TableRow(
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
           child: Text(label, style: TextStyle(color: secondaryColor)),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
-          child: Text(':',
-              style:
-                  TextStyle(color: secondaryColor)), // Colon in its own column
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Text(':', style: TextStyle(color: secondaryColor)),
         ),
         Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 4.0), // Vertical space
-            child: Text(value,
-                style: TextStyle(
-                  color: clr,
-                  decoration: TextDecoration.underline,
-                ))),
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Text(
+            value,
+            style: TextStyle(
+              color: clr,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ],
     );
   }
