1a4080b36dc1ad60ef005423351cf2602d2258ca
Eishal Cyril <eishalcyril@cufa.ie>
2025-03-01 15:03:16 +0530

    endpoint

--- lib/common/network/endpoints.dart
+++ lib/common/network/endpoints.dart
@@ -1,9 +1,9 @@
 class Endpoints {
   Endpoints._();
 
-  static const String baseURL = 'http://100.116.148.125:5000';
-  // static const String baseURL = 'http://10.0.2.2:7190';
+  // static const String baseURL = 'http://100.116.148.125:5000';
+  static const String baseURL = 'http://10.0.2.2:7190';
   
-  // static const String webSocketURL = 'ws://10.0.2.2:7190/api/';
-  static const String webSocketURL = 'ws://100.116.148.125:5000/api/';
+  static const String webSocketURL = 'ws://10.0.2.2:7190/api/';
+  // static const String webSocketURL = 'ws://100.116.148.125:5000/api/';
 }
